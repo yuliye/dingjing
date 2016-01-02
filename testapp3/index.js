abed66da97c8ab7f64913611d2fd76297ee397b0
Yuli Ye <yuli.ye@gmail.com>
2016-01-03 06:47:08 +0800

    Adding stormpath to testapp3

--- testapp3/index.js
+++ testapp3/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
 var app = express();
+
+app.use(stormpath.init(app, {
+  website: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,8 +18,10 @@ app.get('/', function(request, response) {
   response.render('pages/landing');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.on('stormpath.ready', function() {
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+});  
 
 
