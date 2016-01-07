3e0d48c7de96fc4ebba7950ccbac4e05981b9c6a
Yuli Ye <yuli.ye@gmail.com>
2016-01-07 13:54:51 +0800

    a major commit to clean up the code base

--- testapp3/index.js
+++ testapp3/index.js
@@ -18,10 +18,58 @@ app.get('/', function(request, response) {
   response.render('pages/landing');
 });
 
+
+
+
+app.get('/test1', function(request, response) {
+  response.render('pages/bs_detail_view');
+});
+
+var data = [
+        ['Bloody Mary', 3, 7, 11, 23, 11, 3, 7, 11, 23, 11, 43, 33],
+        ['Martini', 5, 7, 11, 23, 11, 3, 7, 11, 23, 11, 3, 7],
+        ['Scotch', 11, 7, 11, 23, 11, 7, 11, 23, 11, 3, 7, 11],
+        ['Bloody Mary', 3, 7, 11, 23, 11, 3, 7, 11, 23, 11, 43, 33],
+        ['Martini', 5, 7, 11, 23, 11, 3, 7, 11, 23, 11, 3, 7],
+        ['Scotch', 11, 7, 11, 23, 11, 7, 11, 23, 11, 3, 7, 11],
+        ['Bloody Mary', 3, 7, 11, 23, 11, 3, 7, 11, 23, 11, 43, 33],
+        ['Martini', 5, 7, 11, 23, 11, 3, 7, 11, 23, 11, 3, 7],
+        ['Scotch', 11, 7, 11, 23, 11, 7, 11, 23, 11, 3, 7, 11],
+    ];
+
+var header = ['基金名', 
+              '上月回报', 
+              '上年回报', 
+              '最大回撤', 
+              '年复回报', 
+              '最低投资',  
+              '资产规模', 
+              '管理费用', 
+              '业绩提成', 
+              '行业侧重', 
+              '投资策略',
+              '决策类型',
+              '基金组成']
+
+var pageIndex = [1, 5, 9]
+
+app.get('/test2', function(request, response) {
+  response.render('pages/bs_list_view',{
+    header: header, 
+    data: data, 
+    pageIndex: pageIndex});
+});
+
+app.get('/test3', function(request, response) {
+  response.render('pages/bs_search_view');
+});
+
+app.get('/public/:name?', function(request, response) {
+  response.render('pages/short_header',{title:request.params.name});
+});
+
 app.on('stormpath.ready', function() {
   app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
 });  
-
-
